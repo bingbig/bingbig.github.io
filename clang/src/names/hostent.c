4d0b419995afaa34c571278b91bdaa0151c4eabe
bingbig <598604711@qq.com>
2019-01-08 23:50:01 +0800

    remove warning

--- clang/src/names/hostent.c
+++ clang/src/names/hostent.c
@@ -2,18 +2,23 @@
 
 int main(int argc, char const *argv[])
 {
-    char *ptr;
+    char **pptr;
 
     struct hostent *hptr;
 
     while(--argc){
-        ptr = *++argv;
-        if((hptr = gethostbyname(ptr)) == NULL){
+        ++argv;
+        if ((hptr = gethostbyname(*argv)) == NULL)
+        {
             err_msg("gethostname error for hosrt: %s: %s", ptr, hstrerror(h_errno));
             continue;
         }
 
         printf("official hostname: %s\n", hptr->h_name);
+
+        for (pptr = hptr->h_aliases; *pptr != NULL; ++pptr)
+            printf("\t alias: %s\n", *pptr);
+
     }
     return 0;
 }
