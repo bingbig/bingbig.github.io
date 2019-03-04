4664f418bb8918b629152b86d0c77630e44b2525
bingbig <598604711@qq.com>
2019-03-04 21:54:20 +0800

    update

--- datastructure/bstree.c
+++ datastructure/bstree.c
@@ -34,6 +34,21 @@ void insert(BSTree **ptr, int item)
     
 }
 
+BSTree *search(BSTree *bst, int item)
+{
+    BSTree *p = bst;
+    while(p != NULL){
+        if(p->data == item)
+            return p;
+        else if(p->data > item)
+            p = p->ltree;
+        else
+            p = p->rtree;
+    }
+
+    return NULL;
+}
+
 /**
  * 中序遍历
  */ 
@@ -58,5 +73,7 @@ int main(int argc, char const *argv[])
     insert(&bst, 19);
     insert(&bst, 20);
     inOrderPrint(bst);
+    printf("\n");
+    inOrderPrint(search(bst, 10));
     return 0;
 }
